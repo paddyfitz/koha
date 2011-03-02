db55ca767ee798d0f3075918f8b3d3d20bb3715a
ptfs-euro at hpl <colin.campbell@ptfs-europe.com>
2011-03-02 22:13:21 +0000

    Make all SIP Checkins OK

--- C4/SIP/Sip/MsgType.pm
+++ C4/SIP/Sip/MsgType.pm
@@ -635,7 +635,7 @@ sub handle_checkin {
     $patron = $status->patron;
     $item   = $status->item;
 
-    $resp .= $status->ok ? '1' : '0';
+    $resp .= '1';
     $resp .= $status->resensitize ? 'Y' : 'N';
     if ($item && $ils->supports('magnetic media')) {
 		$resp .= sipbool($item->magnetic_media);
