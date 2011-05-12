ef3040dd7f197da3424fd5f897df24453e348627
Ian Bayes <ian.bayes@ptfs-europe.com>
2011-05-12 22:47:32 +0100

    (Halton) Sip restart port script

---
+++ C4/SIP/sip_restart_port.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. $HOME/.bash_profile
+
+
+target="SIPServer.$1.xml";
+PROCPID=$(ps x -o pid,ppid,args --sort ppid | grep "^[^ ]*  *[0-9]*  *1  *.*$target" | grep -v grep | awk '{print $1}');
+
+if [ ! $PROCPID ] ; then
+    echo "No processes found for $target";
+    exit;
+fi
+
+echo "SIP Processes for this user ($USER):";
+ps x -o pid,ppid,args --sort ppid | grep "$target" | grep -v grep ;
+for x in $PROCPID
+do
+    echo "Killing process #$x";
+    kill $x;
+done
+echo /home/koha/kohaclone/C4/SIP/sip_run.sh ./SIPServer.$1.xml /home/koha/koha-dev/var/log/sip_$1.out /home/koha/koha-dev/var/log/sip_$1.err
+/home/koha/kohaclone/C4/SIP/sip_run.sh ./SIPServer.$1.xml /home/koha/koha-dev/var/log/sip_$1.out /home/koha/koha-dev/var/log/sip_$1.err
