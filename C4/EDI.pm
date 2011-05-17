cc1d6ebdab3f85b4bf3a7fd3ce33ab14e4f8500a
Mark Gavillet <mark.gavillet@ptfs-europe.com>
2011-05-17 16:37:27 +0100

    Fixed missing semicolon from line 398 in EDI.pm

--- C4/EDI.pm
+++ C4/EDI.pm
@@ -395,7 +395,7 @@ sub CreateEDIOrder {
 			print EDIORDER "IMD+L+230+:::$callnumber'";													# shelfmark
 		}
 		print EDIORDER "QTY+21:$quantity'";																# quantity
-		print EDIORDER "GIR+001+$branchcode:LLO+$halton_collection:LFN+"								# branchcode, sequence or collection code
+		print EDIORDER "GIR+001+$branchcode:LLO+$halton_collection:LFN+";								# branchcode, sequence or collection code
 		if ($callnumber ne '')
 		{
 			print EDIORDER "641.594:LCL+";																# shelfmark
