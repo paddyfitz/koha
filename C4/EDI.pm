paddyfitz/koha

commit 32f016c4acdbb9c007e2b5420856fb129a587566

EDI - Added ISBN validation for order messages. If both 10 and 13 digit ISBNs are returned for the item (i.e. $item->isbn = '9780123456789 | 0123456789') the first ISBN is converted to a 13 digit ISBN and returned in the order message. If $item->isbn contains an invalid value, 0 is returned.

C4/EDI.pm
@@ -371,9 +371,21 @@ sub CreateEDIOrder {
 		$price=sprintf "%.2f",$item->{listprice};
 		my $isbn=cleanisbn($item->{isbn});
 		$isbn=Business::ISBN->new($isbn);
-		if ($isbn ne '')
+		if ($isbn)
 		{
-			$isbn=$isbn->as_isbn13;
+			if ($isbn->is_valid)
+			{
+				$isbn=($isbn->as_isbn13)->isbn;
+				
+			}
+			else
+			{
+				$isbn="0";
+			}
+		}
+		else
+		{
+			$isbn=0;
 		}
 		my $copyrightdate=escape($item->{publicationyear});
 		my $quantity=escape($item->{quantity});
@@ -465,6 +477,12 @@ sub CreateEDIOrder {
 			{
 				$isbn=substr($isbn,0,($i-1));
 			}
+			if (index($isbn,"|") !=-1)
+			{
+				my @isbns=split(/\|/,$isbn);
+				$isbn=$isbns[0];
+				#print "0: ".$isbns[0]."\n";
+			}
 			$isbn=escape($isbn);
 			$isbn =~ s/^\s+//;
 			$isbn =~ s/\s+$//;
@@ -472,7 +490,7 @@ sub CreateEDIOrder {
 		}
 		else
 		{
-			return;
+			return undef;
 		}
 	}
 	

commit ce6156696e50ac5e22d44772308b82ea904db122

EDI - correction for bug introduced in commit 32f016c4acdbb9c007e2b5420856fb129a587566

C4/EDI.pm
@@ -401,8 +401,8 @@ sub CreateEDIOrder {
 		my ($branchcode,$callnumber,$itype,$lsqccode,$fund) = GetOrderItemInfo($item->{'ordernumber'});
 		$callnumber=escape($callnumber);
 		
-		print EDIORDER "LIN+$linecount++".$isbn->isbn.":EN'";											# line number, isbn
-		print EDIORDER "PIA+5+".$isbn->isbn.":IB'";														# isbn as main product identification
+		print EDIORDER "LIN+$linecount++".$isbn.":EN'";											# line number, isbn
+		print EDIORDER "PIA+5+".$isbn.":IB'";														# isbn as main product identification
 		print EDIORDER "IMD+L+050+:::$title'";															# title
 		print EDIORDER "IMD+L+009+:::$author'";															# full name of author
 		print EDIORDER "IMD+L+109+:::$publisher'";														# publisher