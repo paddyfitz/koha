e0e853b281a1b60c8c34d8085b6b3f05a20c6b9c
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2011-06-24 16:51:05 +0100

    Removed hard-coded dollar sign from "Patron owes" message

--- C4/Members.pm
+++ C4/Members.pm
@@ -460,7 +460,7 @@ sub patronflags {
     if ( $amount > 0 ) {
         my %flaginfo;
         my $noissuescharge = C4::Context->preference("noissuescharge") || 5;
-        $flaginfo{'message'} = sprintf "Patron owes \$%.02f", $amount;
+        $flaginfo{'message'} = sprintf "Patron owes %.02f", $amount;
         $flaginfo{'amount'}  = sprintf "%.02f", $amount;
         if ( $amount > $noissuescharge && !C4::Context->preference("AllowFineOverride") ) {
             $flaginfo{'noissues'} = 1;
