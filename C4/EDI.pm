paddyfitz/koha

commit b4c41852a5dacfba8cf8f95dc7dd35b1965f3eff

EDI - Added support for EAN number in product identifier lines

C4/EDI.pm
@@ -369,23 +369,30 @@ sub CreateEDIOrder {
 		my $author=string35escape(escape($item->{author}));
 		my $publisher=string35escape(escape($item->{publishercode}));
 		$price=sprintf "%.2f",$item->{listprice};
-		my $isbn=cleanisbn($item->{isbn});
-		$isbn=Business::ISBN->new($isbn);
-		if ($isbn)
+		my $isbn;
+		if (length($item->{isbn})==10 || substr($item->{isbn},0,3) eq "978")
 		{
-			if ($isbn->is_valid)
+			$isbn=cleanisbn($item->{isbn});
+			$isbn=Business::ISBN->new($isbn);
+			if ($isbn)
 			{
-				$isbn=($isbn->as_isbn13)->isbn;
-				
+				if ($isbn->is_valid)
+				{
+					$isbn=($isbn->as_isbn13)->isbn;
+				}
+				else
+				{
+					$isbn="0";
+				}
 			}
 			else
 			{
-				$isbn="0";
+				$isbn=0;
 			}
 		}
 		else
 		{
-			$isbn=0;
+			$isbn=$item->{isbn};
 		}
 		my $copyrightdate=escape($item->{publicationyear});
 		my $quantity=escape($item->{quantity});

commit 52cd69e38972ce67da036491a244f4fb98b10212

EDI - fixed bug introduced in b4c41852a5dacfba8cf8f95dc7dd35b1965f3eff Marc records containing more than one ISBN now process correctly

C4/EDI.pm
@@ -370,7 +370,7 @@ sub CreateEDIOrder {
 		my $publisher=string35escape(escape($item->{publishercode}));
 		$price=sprintf "%.2f",$item->{listprice};
 		my $isbn;
-		if (length($item->{isbn})==10 || substr($item->{isbn},0,3) eq "978")
+		if (length($item->{isbn})==10 || substr($item->{isbn},0,3) eq "978" || index($item->{isbn},"|") !=-1)
 		{
 			$isbn=cleanisbn($item->{isbn});
 			$isbn=Business::ISBN->new($isbn);