7b2cd5f45a0ea23435a015dca51c5c7e55a99e88
Colin Campbell <colin.campbell@ptfs-europe.com>
2012-11-09 12:22:08 +0000

    Remove unused variables from C4::Housebound
    localized lexical vars to where they are used
    Avoided unnecessary method calls
    Remove a finish
    No need to interpolate to constant strings
    Fix some violations of perlstyle

--- C4/Housebound.pm
+++ C4/Housebound.pm
@@ -23,7 +23,6 @@ use C4::Context;
 use parent qw(Exporter);
 
 our $VERSION = 3.02;
-our $debug   = $ENV{DEBUG} || 0;
 our @EXPORT  = qw(
   GetHouseboundDetails
   CreateHouseboundDetails
@@ -75,14 +74,12 @@ $notes				- freetext notes containing useful information about the patron (e.g. 
 
 sub GetHouseboundDetails {
     my ($borrowernumber) = @_;
-    my $dbh = C4::Context->dbh;
-    my $query;
-    my $sth;
     if ($borrowernumber) {
-        $sth = $dbh->prepare("select * from housebound where borrowernumber=?");
+        my $dbh = C4::Context->dbh;
+        my $sth =
+          $dbh->prepare('select * from housebound where borrowernumber=?');
         $sth->execute($borrowernumber);
-        my $housebound = $sth->fetchrow_hashref;
-        return $housebound;
+        return $sth->fetchrow_hashref;
     }
     return;
 }
@@ -91,12 +88,10 @@ sub CreateHouseboundDetails {
     my ( $borrowernumber, $day, $frequency, $Itype_quant, $Item_subject,
         $Item_authors, $referral, $notes )
       = @_;
-    my $dbh = C4::Context->dbh;
-    my $query;
-    my $sth;
     if ($borrowernumber) {
-        $sth = $dbh->prepare(
-"insert into housebound (day, frequency, borrowernumber, Itype_quant, Item_subject, Item_authors, referral, notes) values (?,?,?,?,?,?,?,?)"
+        my $dbh = C4::Context->dbh;
+        my $sth = $dbh->prepare(
+'insert into housebound (day, frequency, borrowernumber, Itype_quant, Item_subject, Item_authors, referral, notes) values (?,?,?,?,?,?,?,?)'
         );
         $sth->execute( $day, $frequency, $borrowernumber, $Itype_quant,
             $Item_subject, $Item_authors, $referral, $notes );
@@ -105,16 +100,15 @@ sub CreateHouseboundDetails {
 }
 
 sub UpdateHouseboundDetails {
-    my ($hbnumber,     $borrowernumber, $day,
+    my (
+        $hbnumber,     $borrowernumber, $day,
         $frequency,    $Itype_quant,    $Item_subject,
         $Item_authors, $referral,       $notes
     ) = @_;
-    my $dbh = C4::Context->dbh;
-    my $query;
-    my $sth;
     if ($hbnumber) {
-        $sth = $dbh->prepare(
-"update housebound set day=?,frequency=?,borrowernumber=?,Itype_quant=?,Item_subject=?,Item_authors=?,referral=?,notes=? where hbnumber=?"
+        my $dbh = C4::Context->dbh;
+        my $sth = $dbh->prepare(
+'update housebound set day=?,frequency=?,borrowernumber=?,Itype_quant=?,Item_subject=?,Item_authors=?,referral=?,notes=? where hbnumber=?'
         );
         $sth->execute(
             $day,         $frequency,    $borrowernumber,
@@ -127,14 +121,10 @@ sub UpdateHouseboundDetails {
 
 sub GetCurrentHouseboundInstanceList {
     my ($borrowernumber) = @_;
-    my $dbh = C4::Context->dbh;
-    my $query;
-    my $sth;
     if ($borrowernumber) {
-
-#$sth = $dbh->prepare("select * from housebound_instance where borrowernumber=?");
-        $sth = $dbh->prepare(
-"SELECT housebound_instance . * , concat( volunteer.firstname, ' ', volunteer.surname ) AS vol, concat(chooser.firstname, ' ', chooser.surname) as cho, concat(deliverer.firstname, ' ', deliverer.surname) as del FROM housebound_instance left JOIN borrowers volunteer ON volunteer.borrowernumber = housebound_instance.volunteer left join borrowers chooser on chooser.borrowernumber = housebound_instance.chooser left join borrowers deliverer on deliverer.borrowernumber = housebound_instance.deliverer where housebound_instance.borrowernumber=? order by housebound_instance.dmy desc"
+        my $dbh = C4::Context->dbh;
+        my $sth = $dbh->prepare(
+q|SELECT housebound_instance . * , concat( volunteer.firstname, ' ', volunteer.surname ) AS vol, concat(chooser.firstname, ' ', chooser.surname) as cho, concat(deliverer.firstname, ' ', deliverer.surname) as del FROM housebound_instance left JOIN borrowers volunteer ON volunteer.borrowernumber = housebound_instance.volunteer left join borrowers chooser on chooser.borrowernumber = housebound_instance.chooser left join borrowers deliverer on deliverer.borrowernumber = housebound_instance.deliverer where housebound_instance.borrowernumber=? order by housebound_instance.dmy desc|
         );
         $sth->execute($borrowernumber);
         my $housebound_instances = $sth->fetchall_arrayref( {} );
@@ -145,11 +135,9 @@ sub GetCurrentHouseboundInstanceList {
 
 sub GetVolunteerNameAndID {
     my ($borrowernumber) = @_;
-    my $dbh = C4::Context->dbh;
-    my $query;
-    my $sth;
     if ($borrowernumber) {
-        $sth = $dbh->prepare(
+        my $dbh = C4::Context->dbh;
+        my $sth = $dbh->prepare(
 "select borrowernumber,title,firstname,surname from borrowers where borrowernumber=?"
         );
         $sth->execute($borrowernumber);
@@ -161,27 +149,21 @@ sub GetVolunteerNameAndID {
 
 sub GetVolunteerList {
     my $dbh = C4::Context->dbh;
-    my $query;
-    my $sth;
-    $sth = $dbh->prepare(
-"SELECT borrowernumber as volbornumber, concat(title, ' ', firstname, ' ', surname) as fullname from borrowers where categorycode='VOL' order by surname, firstname asc"
+    my $sth = $dbh->prepare(
+q|SELECT borrowernumber as volbornumber, concat(title, ' ', firstname, ' ', surname) as fullname from borrowers where categorycode='VOL' order by surname, firstname asc|
     );
     $sth->execute();
-    my $volunteers = $sth->fetchall_arrayref( {} );
-    return $volunteers;
+    return $sth->fetchall_arrayref( {} );
 }
 
 sub GetHouseboundInstanceDetails {
     my ($instanceid) = @_;
-    my $dbh = C4::Context->dbh;
-    my $query;
-    my $sth;
     if ($instanceid) {
-        $sth =
-          $dbh->prepare("SELECT * from housebound_instance where instanceid=?");
+        my $dbh = C4::Context->dbh;
+        my $sth =
+          $dbh->prepare('SELECT * from housebound_instance where instanceid=?');
         $sth->execute($instanceid);
-        my $instancedetails = $sth->fetchrow_hashref;
-        return $instancedetails;
+        return $sth->fetchrow_hashref;
     }
 
     # return undef if no instanceid
@@ -192,12 +174,10 @@ sub UpdateHouseboundInstanceDetails {
     my ( $instanceid, $hbnumber, $dmy, $time, $borrowernumber, $volunteer,
         $chooser, $deliverer )
       = @_;
-    my $dbh = C4::Context->dbh;
-    my $query;
-    my $sth;
     if ($hbnumber) {
-        $sth = $dbh->prepare(
-"update housebound_instance set hbnumber=?, dmy=?, time=?, borrowernumber=?, volunteer=?, chooser=?, deliverer=? where instanceid=?"
+        my $dbh = C4::Context->dbh;
+        my $sth = $dbh->prepare(
+'update housebound_instance set hbnumber=?, dmy=?, time=?, borrowernumber=?, volunteer=?, chooser=?, deliverer=? where instanceid=?'
         );
         $sth->execute( $hbnumber, $dmy, $time, $borrowernumber, $volunteer,
             $chooser, $deliverer, $instanceid );
@@ -209,12 +189,10 @@ sub CreateHouseboundInstanceDetails {
     my ( $hbnumber, $dmy, $time, $borrowernumber, $volunteer, $chooser,
         $deliverer )
       = @_;
-    my $dbh = C4::Context->dbh;
-    my $query;
-    my $sth;
     if ($borrowernumber) {
-        $sth = $dbh->prepare(
-"insert into housebound_instance (hbnumber, dmy, time, borrowernumber, volunteer, chooser, deliverer) values (?,?,?,?,?,?,?)"
+        my $dbh = C4::Context->dbh;
+        my $sth = $dbh->prepare(
+'insert into housebound_instance (hbnumber, dmy, time, borrowernumber, volunteer, chooser, deliverer) values (?,?,?,?,?,?,?)'
         );
         $sth->execute( $hbnumber, $dmy, $time, $borrowernumber, $volunteer,
             $chooser, $deliverer );
@@ -224,39 +202,34 @@ sub CreateHouseboundInstanceDetails {
 
 sub DeleteHouseboundInstanceDetails {
     my ($instanceid) = @_;
-    my $dbh = C4::Context->dbh;
-    my $query;
-    my $sth;
     if ($instanceid) {
-        $sth =
-          $dbh->prepare("delete from housebound_instance where instanceid=?");
+        my $dbh = C4::Context->dbh;
+        my $sth =
+          $dbh->prepare('delete from housebound_instance where instanceid=?');
         $sth->execute($instanceid);
     }
     return;
 }
 
 sub CheckPrevIssue {
-	my ($borrowernumber,$biblionumber) = @_;
-	my $dbh = C4::Context->dbh;
-    my $query;
-    my $item;
-    my $sth;
-    my $previssue=0;
-    $sth = $dbh->prepare("select itemnumber from items where biblionumber=?");
+    my ( $borrowernumber, $biblionumber ) = @_;
+    my $dbh       = C4::Context->dbh;
+    my $previssue = 0;
+    my $sth =
+      $dbh->prepare('select itemnumber from items where biblionumber=?');
     $sth->execute($biblionumber);
-    while(my @row=$sth->fetchrow_array())
-    {
-    	$query = $dbh->prepare("select count(itemnumber) from old_issues where borrowernumber=? and itemnumber=?");
-    	$query->execute($borrowernumber,$row[0]);
-    	while(my @matches=$query->fetchrow_array())
-    	{
-    		if ($matches[0]>0)
-    		{
-    			$previssue=1;
-    		}
-    	}
+
+    my $query = $dbh->prepare(
+'select count(itemnumber) from old_issues where borrowernumber=? and itemnumber=?'
+    );
+    while ( my @row = $sth->fetchrow_array() ) {
+        $query->execute( $borrowernumber, $row[0] );
+        while ( my @matches = $query->fetchrow_array() ) {
+            if ( $matches[0] > 0 ) {
+                $previssue = 1;
+            }
+        }
     }
-    $sth->finish;
     return $previssue;
 }
 
